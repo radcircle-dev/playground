a20c9a0c2a5444e5b59992feb0914e3b28941a4b
Aaron <ajammon1@gmail.com>
2014-09-17 03:32:40 -0100

    search box
    jquery should be working

---
+++ search.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+     
+    $('#global-radcircle-search-bar').click(function(){
+        $(this).siblings('.search-dropdown').toggleClass('hidden');
+         });
+    
+    
+   });  
+
