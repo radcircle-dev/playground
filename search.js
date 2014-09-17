c3560fea9f60460202884629c805b790e04b7443
Aaron <ajammon1@gmail.com>
2014-09-17 05:55:15 -0100

    changes
    changes

--- search.js
+++ search.js
@@ -3,7 +3,5 @@ $(document).ready(function(){
     $('#global-radcircle-search-bar').click(function(){
         $(this).siblings('.search-dropdown').toggleClass('hidden');
          });
-    
-    
-   });  
+     });  
 
