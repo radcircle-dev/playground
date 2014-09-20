32a4e036e1059e8a1f7c58a25e692fd5fc87ae31
Aaron <ajammon1@gmail.com>
2014-09-20 17:56:02 -0100

    almost there
    contact us/ submit music

--- js/search.js
+++ js/search.js
@@ -2,6 +2,11 @@ $(document).ready(function(){
      
     $('#global-radcircle-search-bar').click(function(){
         $(this).siblings('.search-dropdown').toggleClass('hidden');
-         });
-     });  
+        
+    
+     $('.search-dropdown input').focus();
+    
+    
+    });
+ });  
 
